065dc90535b9b5fe1ee6465f7cad0348851de274
trungls1706 <trungls1706@gmail.com>
2022-12-30 14:29:57 +0700

    init

---
+++ client.js
@@ -0,0 +1,18 @@
+const eventSource = new EventSource('http://localhost:8000')
+
+function updateMessage (message) {
+  const list = document.getElementById('messages')
+  const item = document.createElement('p')
+  item.textContent = message
+  list.appendChild(item)
+}
+
+eventSource.onmessage = function (event) {
+  console.log('event',event)
+  updateMessage(event.data)
+}
+
+eventSource.onerror = function () {
+  updateMessage('Server closed connection')
+  eventSource.close()
+}
